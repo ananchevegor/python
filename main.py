e6cfe66191edaf06a4ffd13295daad8170ecb61b
ananchevegor <84960978+ananchevegor@users.noreply.github.com>
2022-02-19 14:44:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-from bs4 import BeautifulSoup
-import requests
-from lxml import etree
-import lxml.html
-import csv
+# распознавание лиц
+import cv2
 
+def webcamera():
 
-def parse(url):
-    api = requests.get(url)
-    tree = lxml.html.document_fromstring(api.text)
-    the_vote = tree.xpath('//*[@id="dle-vote"]/div[1]/text()')
-    print(the_vote)
+    cam = cv2.VideoCapture(0)
 
+    while True:
+        ret, img = cam.read()
+        cv2.imshow('camera', img)
+        if cv2.waitKey(10) == 27:
+            break
+    cam.realise()
+    cv2.destroyAllWindows()
 
-def main():
-    parse("http://dh-online.ru/sezon-6/130-6-sezon-18-seriya-pavshiy-rycar.html")
-
-
-if __name__ == "__main__":
-    main()
-
-def idk():
-    url_html = "http://dh-online.ru/sezon-6/130-6-sezon-18-seriya-pavshiy-rycar.html"
-    with open('url_html', 'r') as html_file:
-        content = html_file.read()
-
-        soap = BeautifulSoup
-
-        print(content)
+webcamera()
